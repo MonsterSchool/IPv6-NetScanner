7b86b4d4c65763a9aec32f33d3211275f09640ba
Max <36601278+maxlluky@users.noreply.github.com>
2021-10-12 09:05:38 +0200

    Update PacketBuilder.cs

--- src/Classes/PacketBuilder.cs
+++ src/Classes/PacketBuilder.cs
@@ -64,4 +64,31 @@ class PacketBuilder
         }
         return null;
     }    
+    
+    private ushort ComputeChecksum(byte[ ] payLoad)
+    {
+        uint xsum = 0;
+        ushort shortval = 0, hiword = 0, loword = 0;
+ 
+        // Sum up the 16-bits
+        for (int i = 0; i < payLoad.Length / 2; i++)
+        {
+            hiword = (ushort)(((ushort)payLoad[i * 2]) << 8);
+            loword = (ushort)payLoad[(i * 2) + 1];
+            shortval = (ushort)(hiword | loword);
+            xsum = xsum + (uint)shortval;
+        }
+        // Pad if necessary
+
+        if ((payLoad.Length % 2) != 0)
+        {
+            xsum += (uint)payLoad[payLoad.Length - 1];
+        }
+ 
+        xsum = ((xsum >> 16) + (xsum & 0xFFFF));
+        xsum = (xsum + (xsum >> 16));
+        shortval = (ushort)(~xsum);
+ 
+        return shortval;
+    }
 }
